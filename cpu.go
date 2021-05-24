d212f139fff65bb6d1ac46142d61c3342d151106
Nicholas Rodgers <nrodgers88@gmail.com>
2021-05-23 23:52:22 -0400

    implemented interpeter for opcodes 1NNN to 7xNN

--- cpu.go
+++ cpu.go
@@ -25,14 +25,9 @@ type Chip8 struct {
 
 
 //Fetch two bytes (one opcode) from memory and return the opcode
-func(c *Chip8) Fetch(index uint16)uint16{
-	return c.Memory[index] << 8 | c.Memory[index+1]
+func(c *Chip8) Fetch(index uint8)uint16{
+	return uint16(c.Memory[index]) << 8 | uint16(c.Memory[index+1])
 }
-
-//need to implement
-func(c *Chip8) Decode(){
-}
-
 
 
 func(c *Chip8) Init(){
@@ -41,4 +36,60 @@ func(c *Chip8) Init(){
 	c.Index = 0
 	c.Sp = 0 */
 
+}
+
+func(c *Chip8) Cycle(){
+	c.Opcde = c.Fetch(c.pc)
+
+	switch c.Opcode & 0xF000{
+
+		//jumps to address NNN
+		case 0x1000:
+			c.PC = c.Opcode & 0x0FFF
+
+		//calls subroutine at NNN
+		case 0x2000:
+			c.Stack[c.Sp] =c.PC
+			c.Sp += 1
+			c.PC = c.Opcode & 0x0FFF
+		
+		case 0x3000:
+			// skips the next instruction if the  register does not equal the last 8 bits in the opcode
+			if uint16([c.Register[(c.Opcode & 0x0F00) >> 8]) == c.Opcode & 0x00ff {
+				c.PC += 4
+			} else{
+				c.PC += 2
+			}
+
+		case 0x4000:
+			// skips the next instruction if the  register does not equal the last 8 bits in the opcode
+			if uint16([c.Register[(c.Opcode & 0x0F00) >> 8]) != c.Opcode & 0x00ff {
+				c.PC += 4
+			} else{
+				c.PC += 2
+			}
+
+		case 0x5000:
+			x := c.Opcode & 0x0F00
+			y := c.Opcode & 0x00F0
+
+			if c.Register[x >> 8] == c.Register[y >> 4]{
+				c.PC += 4
+			} else {
+				c.PC += 2
+			}
+
+		case 0x6000:
+			x := c.Opcode & 0x0F00
+			c.Register[x >>8] == uint8(c.Opcode & 0x00FF)
+			c.Increment()
+
+		case 0x7000:
+			c.Register[c.Opcode & 0x0F00 >> 8] += byte(c.Opcode & 0x00FF)
+			c.Increment()
+
+	}
+	func (c *Chip8) Increment(){
+		c.PC += 2
+	}
 }
