7c5d9541c633850d89bc487f76b9931ebfd152c9
Nicholas Rodgers <nrodgers88@gmail.com>
2021-06-01 23:09:44 -0400

    removed increment and added it in a cycle function.  added 0xfx0a opcode.

--- cpu.go
+++ cpu.go
@@ -3,6 +3,8 @@ package cpu
 import
 	("fmt"
 	"math"
+	"io"
+	"os"
 	)
 
 type Chip8 struct {
@@ -30,6 +32,30 @@ type Chip8 struct {
 	DrawFlag bool
 }
 
+const (
+		STARTADDRESS = 0x200
+		FONTSET_STARTADDRESS = 0x50
+	)
+
+//sprites are represented as 5 bytes where each bit represents a pixel and are reserved in memory from starting at 0x50
+var fontset []byte{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, //0
+	0x20, 0x60, 0x20, 0x20, 0x70, //1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, //2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, //3
+	0x90, 0x90, 0xF0, 0x10, 0x10, //4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, //5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, //6
+	0xF0, 0x10, 0x20, 0x40, 0x40, //7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, //8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, //9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, //A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, //B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, //C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, //D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, //E
+	0xF0, 0x80, 0xF0, 0x80, 0x80, //F
+}
 
 //Fetch two bytes (one opcode) from memory and return the opcode
 func(c *Chip8) Fetch(index uint8)uint16{
@@ -38,19 +64,44 @@ func(c *Chip8) Fetch(index uint8)uint16{
 
 
 func(c *Chip8) Init(){
-	c.PC = 0x200 // program counter starts at this address
+	c.PC = STARTADDRESS // program counter starts at this address
 	/*c.Opcode = 0
 	c.Index = 0
 	c.Sp = 0 */
+	for i, font := range fontset{
+		c.Memory[FONTSET_STARTADDRESS + i] = fontset
+	}
 
 }
 
-func(c *Chip8) Cycle(){
-	c.Opcde = c.Fetch(c.pc)
+func (c *Chip8) Cycle(){
+	c.Opcode = c.Fetch(c.PC)
+	c.PC +=2
+
+	if c.Delay > 0{
+		c.Delay--
+	}
+	
+	if c.SoundTimer > 0{
+		c.Soundtimer--
+	}
+}
+
+func(c *Chip8) ParseOP(){
 	x := (c.Opcode & 0x0F00) >> 8
 	y := (c.Opcode & 0x00F0) >> 4
 
 	switch c.Opcode & 0xF000{
+
+		case 0x0000:
+			switch c.Opcode & 0x00FF{
+				case 0x00e0:
+					c.Graphics = [64*32]byte{}
+
+				case 0x00ee:
+					c.Sp--
+					c.PC = c.Stack[c.Sp] // +2 here??
+			}
 
 		//jumps to address NNN
 		case 0x1000:
@@ -58,44 +109,37 @@ func(c *Chip8) Cycle(){
 
 		//calls subroutine at NNN
 		case 0x2000:
-			c.Sp += 1
 			c.Stack[c.Sp] =c.PC
+			c.Sp++
 			c.PC = c.Opcode & 0x0FFF
+			
 		
 		case 0x3000:
 			// skips the next instruction if the  register does not equal the last 8 bits in the opcode
 			if uint16([c.Register[(c.Opcode & 0x0F00) >> 8]) == c.Opcode & 0x00ff {
-				c.PC += 4
-			} else{
 				c.PC += 2
-			}
+			} 
 
 		case 0x4000:
 			// skips the next instruction if the  register does not equal the last 8 bits in the opcode
 			if uint16([c.Register[(c.Opcode & 0x0F00) >> 8]) != c.Opcode & 0x00ff {
-				c.PC += 4
-			} else{
 				c.PC += 2
-			}
+			} 
 
 		case 0x5000:
 			x := c.Opcode & 0x0F00
 			y := c.Opcode & 0x00F0
 
 			if c.Register[x >> 8] == c.Register[y >> 4]{
-				c.PC += 4
-			} else {
 				c.PC += 2
-			}
+			} 
 
 		case 0x6000:
 			x := c.Opcode & 0x0F00
 			c.Register[x >>8] == uint8(c.Opcode & 0x00FF)
-			c.Increment()
 
 		case 0x7000:
 			c.Register[c.Opcode & 0x0F00 >> 8] += byte(c.Opcode & 0x00FF)
-			c.Increment()
 			
 		case 0x8000:
 			switch c.Opcode & 0x000F{
@@ -103,22 +147,18 @@ func(c *Chip8) Cycle(){
 				case 0x0000:
 					//Store value of Vy into Vx
 					c.Register[x] = c.Register[y]
-					c.Increment()
 
 				case 0x0001:
 					//Vx = Vx OR Vy
 					c.Register[x] = c.Register[x] | c.Register[y]
-					c,Increment()
 
 				case 0x0002:
 					//Vx = Vx AND Vy
 					c.Register[x] = c.Register[x] & c.Register[y]
-					c.Increment()
 
 				case 0x0003:
 					//Vx = Vx XOR Vy
 					c.Register[x] = c.Register[x] ^ c.Register[y]
-					c.Increment()
 
 				case 0x0004:
 					// if Vx + Vy > 255, then set regiters accordingly and store lowest 8 bits in Vx
@@ -129,7 +169,6 @@ func(c *Chip8) Cycle(){
 					} 
 
 					c.Register[x] += c.Register[y]
-					c.Increment()
 
 				case 0x0005:
 					// set VF to 1 if a carry occurs and subtract Vy from Vx
@@ -139,14 +178,12 @@ func(c *Chip8) Cycle(){
 						c.Register[0xF] = 1
 					} 
 					c.Register[x] -= c.Register[y]
-					c.Increment()
 
 
 				case 0x0006:
 					//Set Register F  to the least significant bit of Register x then divide Register x by 2
 					c.Register[0xF] = c.Register[x] & 0x1
 					c.Register[x] >> 1
-					c.Increment()
 
 				case 0x0007:
 				// Set Register x to RegY - RegX and Register y to based on the comparison of Regy > Regx
@@ -156,13 +193,11 @@ func(c *Chip8) Cycle(){
 					}
 
 					c.Register[x] = c.Register[y] - c.Register[x]
-					c.Increment()
 				
 				case 0x00E:
 					// Set Register F to the most significant bit of x and double Register of x
 					c.Register[0xF] = c.Register[x] >> 7 
 					C.Register[x] << 1
-					c.Increment()
 
 
 				default: 
@@ -171,15 +206,13 @@ func(c *Chip8) Cycle(){
 
 		case 0x9000:
 			//Skip on Regx != Regy
-			c.Increment()
 			if c.Register[x] != c.Register[y]{
-				c.Increment()
+				c.PC +=2
 			}
 
 		case 0xA000: 
 			//set index register to the last 12 bits
 			c.Index = c.Opcode & 0x0FFF
-			c.Increment()
 
 		case 0xB000:
 			// 0xBNNN jumps to address NNN plus Register0
@@ -187,7 +220,6 @@ func(c *Chip8) Cycle(){
 
 		case 0xC000:
 			c.Register[x] = uint8(math.Intn(256)) & uint8(c.Opcode &  0x00FF)
-			c.Increment()
 
 		case 0xD000:
 			height := c.Opcode & 0x000f
@@ -204,24 +236,19 @@ func(c *Chip8) Cycle(){
 			}
 
 			c.DrawFlag = true
-			c.Increment()
 
 		
 		case 0xE000:
 			switch c.Opcode & 0x00ff{
 				case 0x009e:
 					if c.Key[c.Register[x]] == 1{
-						c.PC += 4
-					} else{
-						c.Increment()
-					}
+						c.PC += 2
+					} 
 				
 				case 0x00a1:
 					if c.Key[c.Register[x]] == 0{
-						c.PC += 4
-					} else{
-						c.Increment()
-					}
+						c.PC += 2
+					} 
 
 			}
 
@@ -230,46 +257,69 @@ func(c *Chip8) Cycle(){
 				
 				case 0x0007:
 					c.Register[x] = c.Delay
-					c.Increment()
+
+				case 0x000a:
+					for i, key := range c.Key{
+						if c.Key != 0{
+							c.Register[x] = byte(i)
+							break
+						}
+					}
+					c.PC-=2
 
 				case 0x0015:
 					c.Delay = c.Register[x]
-					c.Increment()
 
 				case 0x0018:
 					c.SoundTimer = c.Register[x]
-					c.Increment()
 
 				case 0x001e:
 					c.Index += c.Register[x]
-					c.Increment()
 
 				case 0x0029:
 					c.Index = uint16(c.Register[x]) * 0x5
-					c.Increment()
 
 				case 0x0033:
 					c.Memory[c.Index] = c.Register[x]/100
 					c.Memory[c.Index+1] = (c.Register[x] /10) % 10
 					c.Memory[c.Index+1] = (c.Register[x] /100) % 100
-					c.Increment()
 
 				case 0x0055:
 					for  i:=uint16(0); i< x; i++{
 						c.memory[c.Index + i] = c.Register[i]
 					}
-					c.Increment()
 
 				case 0x0065:
 					for i := uint16(0); i < x; i++{
 						c.Register[i] = c.Memory[c.Index +i]
-						c.Increment()
 					}
 					
 			}
 	}
 }
 
-func (c *Chip8) Increment(){
-	c.PC += 2
+
+func(c *Chip8) LoadROM(file string) error{
+	f, err := os.Open(file)
+	if err != nil{
+		return err
+	}
+
+	stat, err := f.Stat()
+	if err != nil{
+		return err
+	}
+	
+	buf := make([]byte, stat.Size())
+
+	_, err := f.Read(buf)
+	if err != nil || err != io.EOF{
+		return err
+	}
+
+	for i, bytes := range buf{
+		c.Memory[STARTADDRESS + i] = bytes
+	}
+	
+	return nil
 }
